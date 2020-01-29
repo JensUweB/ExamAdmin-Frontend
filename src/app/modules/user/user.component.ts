c04769fc0181fb26e93e01b400ba78faaea1962f
Jens Uwe Becker <jens.becker@root-itsolutions.de>
2020-01-29 16:46:45 +0100

    implementet basic user profile

--- src/app/modules/user/user.component.ts
+++ src/app/modules/user/user.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class UserComponent {
   title = 'Exam-Admin Frontend';
+  // Placeholder
+  user = {
+    firstName: "Max",
+    lastName: "Meier",
+    email: "tester@localhost",
+    martialArts: [{
+      name: "Battojutsu",
+      style: "Shobukan Inyo-Ryu",
+      ranks: [{
+        name: "1. Kyu",
+        number: 11
+      }]
+    }],
+    clubs: [{
+      name: "Bürbacher Spielvereinigung 1909 e.V."
+    }]
+  }
 }
