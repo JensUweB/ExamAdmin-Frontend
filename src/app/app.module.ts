9b25a577852cccf4b09c9a0bab2501a270016839
Jens Uwe Becker <jens.becker@root-itsolutions.de>
2020-01-29 13:59:01 +0100

    not required

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { AuthComponent } from './modules/auth/auth..component';
+import { UserComponent } from './modules/user/user.component';
+import { MartialArtsComponent } from './modules/martialArts/martialArts.component';
+import { ExamComponent } from './modules/exam/exam.component';
+import { ClubComponent } from './modules/club/club.component';
+import { UmbrellaAssocComponent } from './modules/umbrellaAssoc/umbrellaAssoc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AuthComponent,
+    UserComponent,
+    MartialArtsComponent,
+    ExamComponent,
+    ClubComponent,
+    UmbrellaAssocComponent
   ],
   imports: [
     BrowserModule,
