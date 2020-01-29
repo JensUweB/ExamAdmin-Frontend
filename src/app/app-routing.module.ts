7a5420a57d83d29a2b91ea15d0358ef2b702d06a
Jens Uwe Becker <jens.becker@root-itsolutions.de>
2020-01-29 16:47:07 +0100

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './modules/auth/auth..component';
 import { UserComponent } from './modules/user/user.component';
 import { StartComponent } from './modules/start/start.component';
+import { ExamComponent } from './modules/exam/exam.component';
+import { MartialArtsComponent } from './modules/martialArts/martialArts.component';
+import { ClubComponent } from './modules/club/club.component';
 
 
 const routes: Routes = [
   {path: '', component: StartComponent},
   {path: 'auth', component: AuthComponent},
-  {path: 'user', component: UserComponent}
+  {path: 'user', component: UserComponent},
+  {path: 'exams', component: ExamComponent},
+  {path: 'martialArts', component: MartialArtsComponent},
+  {path: 'clubs', component: ClubComponent},
 ];
 
 @NgModule({
