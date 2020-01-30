9de0e9bc4aa27d62f8336016adb264fa4c804050
Jens Uwe Becker <jens.becker@root-itsolutions.de>
2020-01-30 11:00:17 +0100

    init

---
+++ src/app/modules/examResult/examResult.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exam-result',
+  templateUrl: './examResult.component.html',
+  styleUrls: ['./examResult.component.css']
+})
+export class ExamResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
