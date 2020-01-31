80495c140385993fc43f53c07507bc014edf1e52
Jens Uwe Becker <jens.becker@root-itsolutions.de>
2020-01-31 11:27:45 +0100

    Working user profile with query
    User profile is basically working and gets populated from the backend properly.

--- src/app/modules/user/user.component.ts
+++ src/app/modules/user/user.component.ts
@@ -1,27 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+import { Apollo } from 'apollo-angular';
+//import { Observable } from 'rxjs/Observable';
+//import { map } from 'rxjs/operators';
+import gql from 'graphql-tag';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
+
+const getUser = gql`{getUser{_id, firstName, lastName, email, martialArts{name, styleName, ranks{name, number}}, clubs{club{name}}}}`;
+const queryExamResults = gql`query getAllExamResults{getAllExamResults{_id, user, exam, date, passed, martialArt{name, styleName},rank}}`;
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent {
-  title = 'Exam-Admin Frontend';
-  // Placeholder
-  user = {
-    firstName: "Max",
-    lastName: "Meier",
-    email: "tester@localhost",
-    martialArts: [{
-      name: "Battojutsu",
-      style: "Shobukan Inyo-Ryu",
-      ranks: [{
-        name: "1. Kyu",
-        number: 11
-      }]
-    }],
-    clubs: [{
-      name: "Bürbacher Spielvereinigung 1909 e.V."
-    }]
+export class UserComponent implements OnInit{
+  private user;
+  private examResults;
+
+  constructor(private apollo: Apollo, private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    if(localStorage.getItem('token')){
+      console.log('Token found. Querying exam results!');
+      
+      this.apollo.watchQuery<any>({
+        query: queryExamResults
+      }).valueChanges.subscribe((response) => { 
+        this.examResults = response.data.getAllExamResults;
+        this.examResults.forEach(ele => {
+          const date = new Date(ele.date);
+          ele.date = date.toLocaleDateString();
+        });
+        
+        console.log(this.examResults);
+        console.log('Done!');
+      }, (err) => {console.log('GraphQL error: ',JSON.stringify(err.graphQLErrors[0].message))});
+      
+    }
+    this.user = this.authService.user;
+    if(!this.user) this.router.navigate(['/auth']);
+    
   }
 }
