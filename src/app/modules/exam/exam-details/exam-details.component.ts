66f9070dd6469aaf6809b15ee96f8cf1b9308e5f
Jens Uwe Becker <jens.becker@root-itsolutions.de>
2020-02-13 17:02:58 +0100

    Exam unregistter
    You now can unregister from an exam. Oh, and configurable alerts here too!

--- src/app/modules/exam/exam-details/exam-details.component.ts
+++ src/app/modules/exam/exam-details/exam-details.component.ts
@@ -5,9 +5,10 @@ import { User } from '../../models/user.model';
 import { AuthService } from '../../auth/auth.service';
 import gql from 'graphql-tag';
 import { Apollo } from 'apollo-angular';
-import { error } from 'protractor';
+import { Alert } from '../../types/Alert';
 
 const query = gql`mutation registerToExam($examId: String!){registerToExam(examId: $examId)}`;
+const unregister = gql`mutation unregisterFromExam($examId: String!){unregisterFromExam(examId: $examId)}`;
 
 @Component({
   selector: 'app-exam-details',
@@ -17,9 +18,10 @@ const query = gql`mutation registerToExam($examId: String!){registerToExam(examI
 export class ExamDetailsComponent implements OnInit {
  exam: Exam;
  user: User;
+ hasCheckedIn: boolean;
  editExam: boolean;
  clubs;
- errors = [];
+ alerts: Alert[] = [];
 
   constructor(private examService: ExamService, private authService: AuthService, private apollo: Apollo) { }
 
@@ -28,20 +30,50 @@ export class ExamDetailsComponent implements OnInit {
     this.editExam = this.examService.editExam;
     this.user = this.authService.user;
     this.clubs = this.examService.getCurrentClubs();
+
+    this.hasCheckedIn = this.exam.participants.some(user => user._id == this.user._id);
   }
 
-  onParticipate() {
+  onCheckIn() {
     this.apollo.mutate<any>({
       mutation: query,
       variables: {
         examId: this.exam._id,
       }
     }).subscribe(response => {
-      if(response.data) console.log('[Exam] Ok, you now are listed as participant!');
+      if(response.data){ 
+        this.hasCheckedIn = true;
+        this.exam.participants.push(this.user);
+        this.alerts.push({type:"success", message: 'Success! You are now registered as participant.'});
+        console.log('[Exam] Ok, you now are listed as participant!');
+      }
     }, (err) => {
-      if(err.graphQLErrors.length)  this.errors.push('Code: '+err.graphQLErrors[0].message.statusCode + ' - ' + err.graphQLErrors[0].message.error + ' - ' + err.graphQLErrors[0].message.message);
-      else this.errors.push(err);
+      if(err.graphQLErrors[0]) this.alerts.push({type: 'danger', message: err.graphQLErrors[0].message.message});
+      else this.alerts.push({type: 'danger', message: err});
       console.warn('[Exam]: GraphQL Error:',JSON.stringify(err));
     });
+  }
+  onCheckOut() {
+    this.apollo.mutate<any>({
+      mutation: unregister,
+      variables: {
+        examId: this.exam._id,
+      }
+    }).subscribe(response => {
+      if(response.data){ 
+        this.hasCheckedIn = false;
+        this.exam.participants = this.exam.participants.filter(user => user._id != this.user._id);
+        this.alerts.push({type: 'success', message: 'Success! You are now removed from the participants list.'});
+        console.log('[Exam] Ok, you now are listed as participant!');
+      }
+    }, (err) => {
+      if(err.graphQLErrors[0]) this.alerts.push({type: 'danger', message: err.graphQLErrors[0].message.message});
+      else this.alerts.push({type: 'danger', message: err});
+      console.warn('[Exam]: GraphQL Error:',JSON.stringify(err));
+    });
+  }
+
+  close(alert: Alert) {
+    this.alerts.splice(this.alerts.indexOf(alert), 1);
   }
 }
