fb92476e7f45e248b426a1f9f06b27c0defbe802
JensUweB <58864964+JensUweB@users.noreply.github.com>
2020-04-02 16:21:58 +0200

    removed duplicate code and fixed small bug
    The filtering of the examiner martial arts was returning the opposit result. Fixed that. Removed a big chunk of unnessesary duplicate code.

--- src/app/modules/martialArts/martialArts.service.ts
+++ src/app/modules/martialArts/martialArts.service.ts
@@ -2,32 +2,20 @@ import { OnInit, Injectable } from '@angular/core';
 import gql from 'graphql-tag';
 import { Apollo } from 'apollo-angular';
 import { Router } from '@angular/router';
+import { MartialArt } from '../models/martialArt.model';
 
 const maQuery = gql`{getAllMartialArts{_id, name, styleName, description, examiners{firstName, lastName, martialArts{_id, rankName}}, ranks{name, number}}}`;
 
 @Injectable()
 export class MartialArtsService implements OnInit {
     private martialArts: any[];
+    martialArt: MartialArt;
+    editMode: Boolean;
     
     constructor(
         private apollo: Apollo, 
         private router: Router) {
-        this.apollo.watchQuery<any>({
-            query: maQuery,
-            fetchPolicy: 'no-cache'
-          }).valueChanges.subscribe((response) => { 
-            this.martialArts = response.data.getAllMartialArts;
-            console.log('[MAService] Got some data!');
-    
-            this.martialArts.forEach(ma => {
-              ma.isHidden = true;
-              ma.examiners.forEach(examiner => {
-                let result = examiner.martialArts.filter(ele => ele._id.toString() != ma._id.toString());
-                if(result.length) examiner.martialArts = result;
-              });
-            });
-    
-          }, (err) => {console.warn('[MAService] GraphQL error: ',err.graphQLErrors[0].message)});
+        this.fetch();
     }
 
     fetch() {
@@ -41,7 +29,7 @@ export class MartialArtsService implements OnInit {
         this.martialArts.forEach(ma => {
           ma.isHidden = true;
           ma.examiners.forEach(examiner => {
-            let result = examiner.martialArts.filter(ele => ele._id.toString() != ma._id.toString());
+            let result = examiner.martialArts.filter(ele => ele._id.toString() == ma._id.toString());
             if(result.length) examiner.martialArts = result;
           });
         });
@@ -53,5 +41,10 @@ export class MartialArtsService implements OnInit {
 
     getMartialArts() {
         return this.martialArts;
+    }
+
+    setCurrent(ma: MartialArt, editMode: Boolean) {
+      this.martialArt = ma;
+      this.editMode = editMode;
     }
 }
