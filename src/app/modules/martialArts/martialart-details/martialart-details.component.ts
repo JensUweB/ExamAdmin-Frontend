2f8386bef85a6711bae9615be990ed259925c6e6
JensUweB <58864964+JensUweB@users.noreply.github.com>
2020-04-02 14:17:30 +0200

    init

---
+++ src/app/modules/martialArts/martialart-details/martialart-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-martialart-details',
+  templateUrl: './martialart-details.component.html',
+  styleUrls: ['./martialart-details.component.css']
+})
+export class MartialartDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
