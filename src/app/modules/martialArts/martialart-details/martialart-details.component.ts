e42ea67c2478b9ae773b71d82a95ebd53adf78a7
JensUweB <58864964+JensUweB@users.noreply.github.com>
2020-04-02 16:19:42 +0200

    Martial Art Details is working
    You can read everything, but not edit yet. Edit Mode comes a bit later.

--- src/app/modules/martialArts/martialart-details/martialart-details.component.ts
+++ src/app/modules/martialArts/martialart-details/martialart-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MartialArt } from '../../models/martialArt.model';
+import { MartialArtsService } from '../martialArts.service';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-martialart-details',
@@ -6,8 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./martialart-details.component.css']
 })
 export class MartialartDetailsComponent implements OnInit {
+  ma: MartialArt;
+  editMode: Boolean;
 
-  constructor() { }
+  constructor(
+    private maService: MartialArtsService
+  ) {
+    this.ma = maService.martialArt;
+    this.editMode = maService.editMode;
+   }
 
   ngOnInit(): void {
   }
