3f0ed0ae744bb838c7ba879969bed69e85b1f8f9
JensUweB <58864964+JensUweB@users.noreply.github.com>
2020-04-03 15:31:32 +0200

    improved detail view and added edit mode
    Reusing new martial art component for editing martial arts. You also can add and remove examiners in details view.

--- src/app/modules/martialArts/martialart-details/martialart-details.component.ts
+++ src/app/modules/martialArts/martialart-details/martialart-details.component.ts
@@ -1,25 +1,104 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { MartialArt } from '../../models/martialArt.model';
 import { MartialArtsService } from '../martialArts.service';
 import { User } from '../../models/user.model';
+import { Apollo } from 'apollo-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import gql from 'graphql-tag';
+import { Alert } from '../../types/Alert';
+
+const query = gql`mutation addExaminer($maId: String!, $email: String!)
+{addExaminer(maId: $maId, email: $email){_id}}`;
+const queryRemove = gql`mutation removeExaminer($maId: String!, $userId: String!)
+{removeExaminer(maId: $maId, userId: $userId){_id}}`;
 
 @Component({
   selector: 'app-martialart-details',
   templateUrl: './martialart-details.component.html',
-  styleUrls: ['./martialart-details.component.css']
+  styleUrls: ['./martialart-details.component.scss']
 })
 export class MartialartDetailsComponent implements OnInit {
-  ma: MartialArt;
+  @Output() ma: MartialArt;
+  alerts: Alert[] = [];
   editMode: Boolean;
+  examinerForm: FormGroup;
 
   constructor(
-    private maService: MartialArtsService
+    private maService: MartialArtsService,
+    private apollo: Apollo,
+    private fb: FormBuilder,
+    private modalService: NgbModal,
   ) {
     this.ma = maService.martialArt;
     this.editMode = maService.editMode;
+
+    this.examinerForm = this.fb.group({
+      email: ['', Validators.required],
+    });
+   }
+
+   onSubmit() {
+     if(this.examinerForm.valid) {
+      this.apollo.mutate<any>({
+        mutation: query,
+        variables: {
+          maId: this.ma._id,
+          email: this.email.value,
+        },
+      }).subscribe(response => {
+        if (response.data) {
+          this.alerts.push({type:"success", message: 'New examiner was added!'});
+          console.log('[NewMartialArtComp] Done.');
+        }
+      }, (err) => {
+        if(err.graphQLErrors[0]) this.alerts.push({type: 'danger', message: err.graphQLErrors[0].message.message});
+        else this.alerts.push({type: 'danger', message: err});
+        console.warn('[ExamResult] ', JSON.stringify(err));
+      });
+     }
+   }
+
+   onRemove(userId: string) {
+     this.apollo.mutate<any>({
+        mutation: queryRemove,
+        variables: {
+          maId: this.ma._id,
+          userId: userId,
+        },
+      }).subscribe(response => {
+        if (response.data) {
+          this.ma.examiners = this.ma.examiners.filter(user => user._id != userId);
+          this.alerts.push({type:"success", message: 'Examiner was removed!'});
+          console.log('[NewMartialArtComp] Done.');
+        }
+      }, (err) => {
+        if(err.graphQLErrors[0]) this.alerts.push({type: 'danger', message: err.graphQLErrors[0].message.message});
+        else this.alerts.push({type: 'danger', message: err});
+        console.warn('[ExamResult] ', JSON.stringify(err));
+      });
    }
 
   ngOnInit(): void {
   }
 
+  showEdit() {
+    this.editMode = true;
+  }
+
+  cancelEdit() {
+    this.editMode = false;
+  }
+
+  openPopup(content) {
+    this.modalService.open(content);
+  }
+
+  close(alert: Alert) {
+    this.alerts.splice(this.alerts.indexOf(alert));
+  }
+
+  get email() {
+    return this.examinerForm.get('email');
+  }
 }
