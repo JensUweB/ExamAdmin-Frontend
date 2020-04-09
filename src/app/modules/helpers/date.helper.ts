9fb2fae7eb19d2fff5e048bc4ed0df3086689f6c
JensUweB <58864964+JensUweB@users.noreply.github.com>
2020-04-09 22:37:43 +0200

    trying to fix a bug

--- src/app/modules/helpers/date.helper.ts
+++ src/app/modules/helpers/date.helper.ts
@@ -1,5 +1,5 @@
 export function normalizeDate(input: string): string {
     const date = new Date(input);
-    const str = date.toLocaleDateString().split('.');
+    const str: String[] = date.toLocaleDateString().split('.');
     return str[0].padStart(2,'0')+'.'+str[1].padStart(2,'0')+'.'+str[2];
 }
