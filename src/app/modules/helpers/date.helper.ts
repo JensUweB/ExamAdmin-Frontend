649d594f23645c0e903754014a51a89629087bdc
JensUweB <58864964+JensUweB@users.noreply.github.com>
2020-04-09 08:44:32 +0200

    correcting date string
    Now days and months are always two letters long.

--- src/app/modules/helpers/date.helper.ts
+++ src/app/modules/helpers/date.helper.ts
@@ -1,9 +1,5 @@
 export function normalizeDate(input: string): string {
     const date = new Date(input);
-    return date.toLocaleDateString();
-}
-
-export function normalizeDateTime(input: string): string {
-    const date = new Date(input);
-    return date.toLocaleDateString()+' '+date.toLocaleTimeString();
+    const str = date.toLocaleDateString().split('.');
+    return str[0].padStart(2,'0')+'.'+str[1].padStart(2,'0')+'.'+str[2];
 }
