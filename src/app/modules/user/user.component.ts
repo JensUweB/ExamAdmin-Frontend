3e07f954414dbfb3e4fcda3c3c79529cfe7e9342
JensUweB <58864964+JensUweB@users.noreply.github.com>
2020-04-09 08:46:25 +0200

    implemented user update
    Users now can update their profile information like mail or password

--- src/app/modules/user/user.component.ts
+++ src/app/modules/user/user.component.ts
@@ -10,6 +10,8 @@ import { ClubService } from '../club/club.service';
 import { MartialArtsService } from '../martialArts/martialArts.service';
 import { Alert } from '../types/Alert';
 import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
+import { normalizeDate } from '../helpers/date.helper';
 
 const queryExamResults = gql`query getAllExamResults{getAllExamResults{_id, user, exam, date, passed, reportUri , martialArt{name, styleName},rank}}`;
 
@@ -39,6 +41,7 @@ export class UserComponent implements OnInit, OnDestroy{
   constructor(
     private apollo: Apollo,
     private authService: AuthService,
+    private userService: UserService,
     private clubService: ClubService,
     private maService: MartialArtsService,
     private router: Router,
@@ -55,9 +58,20 @@ export class UserComponent implements OnInit, OnDestroy{
       maId: ['', Validators.required],
       rankId: ['', Validators.required],
     });
-    this.clubs = this.clubService.clubs;
+    /*this.clubs = this.clubService.clubs;
     this.clubForm = this.fb.group({
       clubId: ''
+    });*/
+    this.ngOnInit();
+    this.userForm = this.fb.group({
+      firstName: [this.user.firstName, Validators.required],
+      lastName: [this.user.lastName, Validators.required],
+      email: [this.user.email, Validators.required],
+      password: ['', Validators.required],
+      newPassword: '',
+      newPassword2: '',
+      clubs: null,
+      martialArts: null
     });
   }
 
@@ -100,32 +114,46 @@ export class UserComponent implements OnInit, OnDestroy{
     });
   }
 
-  ngOnInit() {
-    if (localStorage.getItem('token')) {
-      console.log('Token found. Querying exam results!');
+  async ngOnInit() {
+    console.log('[UserComp] Querying Data...');
 
-      this.apollo.watchQuery<any>({
-        query: queryExamResults,
-        fetchPolicy: 'no-cache'
-      }).valueChanges.subscribe((response) => {
-        this.examResults = response.data.getAllExamResults;
-        this.examResults.forEach(ele => {
-          const date = new Date(ele.date);
-          ele.date = date.toLocaleDateString();
-        });
-        console.log('[UserComp] Done!');
-      }, (err) => {
-        this.alerts.push({type: 'danger', message: err});
+    this.apollo.watchQuery<any>({
+      query: queryExamResults,
+      fetchPolicy: 'no-cache'
+    }).valueChanges.subscribe((response) => {
+      this.examResults = response.data.getAllExamResults;
+      this.examResults.forEach(ele => {
+        ele.date = normalizeDate(ele.date);
       });
+    }, (err) => {
+      this.alerts.push({type: 'danger', message: err});
+    });
 
-    }
-    this.authSubscription = this.authService.user.subscribe(data => this.user = data);
-    this.maSubscription = this.maService.martialArts.subscribe(data => this.martialArts = data);
+    this.authSubscription = await this.authService.user.subscribe(data => this.user = data);
+    this.maSubscription = await this.maService.martialArts.subscribe(data => this.martialArts = data);
+    console.log('[UserComp] Done!');
   }
 
   showMaDetails(ma) {
     this.maService.setCurrent(ma, false);
     this.router.navigateByUrl('/martialArt-details');
+  }
+
+  async onUserUpdate() {
+    if(this.userForm.valid && this.newPassword.value == this.newPassword2.value) {
+      await this.userService.updateUser({
+        firstName: this.firstName.value,
+        lastName: this.lastName.value,
+        email: this.email.value,
+        password: this.password.value,
+        newPassword: this.newPassword.value
+      });
+      this.authService.loadUser();
+      // Updating this.user object manualy because fetching from authservice doesn't work correctly here
+      this.user.firstName = this.firstName.value;
+      this.user.lastName = this.lastName.value;
+      this.user.email = this.email.value;
+    }
   }
 
   /**
@@ -163,6 +191,31 @@ export class UserComponent implements OnInit, OnDestroy{
   get rankId() {
     return this.maForm.get('rankId');
   }
+
+  get password() {
+    return this.userForm.get('password');
+  }
+
+  get newPassword() {
+    return this.userForm.get('newPassword');
+  }
+
+  get newPassword2() {
+    return this.userForm.get('newPassword2');
+  }
+
+  get firstName() {
+    return this.userForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.userForm.get('lastName');
+  }
+
+  get email() {
+    return this.userForm.get('email');
+  }
+
   close(alert: Alert) {
     this.alerts.splice(this.alerts.indexOf(alert));
   }
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,5 +1,55 @@
+import { AuthService } from "../auth/auth.service";
+import { Apollo } from "apollo-angular";
+import gql from "graphql-tag";
+import { Alert } from "../types/Alert";
+import { Injectable } from '@angular/core';
 
+const updateUserMutation = gql`
+  mutation updateUser(
+    $newPassword: String!
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    updateUser(
+      newPassword: $newPassword
+      input: {
+        firstName: $firstName
+        lastName: $lastName
+        email: $email
+        password: $password
+      }
+    ) {
+      _id
+    }
+  }
+`;
+@Injectable()
 export class UserService {
-    
+  alerts: Alert[] = [];
 
+  constructor(private apollo: Apollo, private authService: AuthService) {}
+
+  /**
+   * 
+   * @param input: { newPassword, firstname, lastName, email, clubs[ ], martialArts[ ] }
+   */
+  async updateUser(input: Object) {
+    await this.apollo
+      .watchQuery<any>({
+        query: updateUserMutation,
+        fetchPolicy: "no-cache",
+        variables: input
+      })
+      .valueChanges.subscribe(
+        (response) => {
+          console.log("[UserService] Done!");
+        },
+        (err) => {
+          this.alerts.push({ type: "danger", message: err });
+          console.error('[UserService]: ',JSON.stringify(err));
+        }
+      );
+  }
 }
