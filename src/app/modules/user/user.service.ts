97bf622eef4b13995722e11d59f9c0935aa96251
JensUweB <58864964+JensUweB@users.noreply.github.com>
2020-05-17 16:32:55 +0200

    moved data fetching into service

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -4,6 +4,8 @@ import gql from "graphql-tag";
 import { Alert } from "../types/Alert";
 import { Injectable } from '@angular/core';
 import { getGraphQLError, logError } from '../helpers/error.helpers';
+import { BehaviorSubject } from 'rxjs';
+import { normalizeDate } from '../helpers/date.helper';
 
 const updateUserMutation = gql`
   mutation updateUser(
@@ -26,11 +28,18 @@ const updateUserMutation = gql`
     }
   }
 `;
+const queryExamResults = gql`query getAllExamResults{getAllExamResults{_id, user, exam, date, passed, reportUri , martialArt{name, styleName},rank}}`;
+
 @Injectable()
 export class UserService {
   alerts: Alert[] = [];
+  private examResultsArray = [];
+  private _examResults: BehaviorSubject<any[]> = new BehaviorSubject([]);
+  public readonly examResults = this._examResults.asObservable();
 
-  constructor(private apollo: Apollo, private authService: AuthService) {}
+  constructor(private apollo: Apollo, private authService: AuthService) {
+    this.fetch();
+  }
 
   printError(err) {
     logError('[UserComponent]',err);
@@ -56,5 +65,20 @@ export class UserService {
           this.printError(err);
         }
       );
+  }
+
+  async fetch() {
+    this.apollo.watchQuery<any>({
+      query: queryExamResults,
+      fetchPolicy: 'no-cache'
+    }).valueChanges.subscribe((response) => {
+      this.examResultsArray = response.data.getAllExamResults;
+      this.examResultsArray.forEach(ele => {
+        ele.date = normalizeDate(ele.date);
+      });
+      this._examResults.next(this.examResultsArray);
+    }, (err) => {
+      this.printError(err);
+    });
   }
 }
